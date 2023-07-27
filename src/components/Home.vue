3347fe5adf37999938bced1b32579d4d2155d65f
wangyouhua <code-brewer@users.noreply.github.com>
2023-07-27 11:59:22 +0800

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <span>首頁的文字</span>
+    <Message />
+  </div>
+</template>
+
+<script>
+import Message from "@/components/Message";
+
+export default {
+  components: {
+    Message,
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+span {
+  color: red;
+}
+
+.home:deep .message {
+  color: red;
+}
+
+// 以下寫法無法被編釋
+/* .home >>> .message {
+  color: red;
+} */
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="message">
+    <span>
+      Message 外層文字
+      <span> Message 內層文字 </span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
